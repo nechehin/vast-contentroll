f12b8911cce06870555eb03eb009d948acbea6db
e.nechehin <nechehin@gmail.com>
2020-05-15 14:36:31 +0300

    hide container on allAdsCompleted

--- vast-contentroll.js
+++ vast-contentroll.js
@@ -35,7 +35,7 @@ this._domObserver&&(this._domObserver.disconnect(),this._domObserver=null))};d.p
 /*!
  * VAST content-roll
  *
- * @version 0.0.12
+ * @version 0.0.13
  * @link https://github.com/nechehin/vast-contentroll
  */
 (function(){
@@ -256,7 +256,7 @@ this._domObserver&&(this._domObserver.disconnect(),this._domObserver=null))};d.p
          */
         function onAdsEmpty(roll) {
             intersectionObserver.unobserve(roll);
-            roll.style.height = '0';
+            roll.style.display = 'none';
 
             var onEmptyCallback = data(roll, 'on-empty');
             if (typeof window[onEmptyCallback] === 'function') {
